6adb9dd10fc2649d127e0ec4275d2f07f2568f75
CheemsBread505 <dogecheemsbread@gmail.com>
2023-05-30 17:00:48 -0400

    Video converts and renders

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+=== video-converter ===
+
+Just a simple website I made that converts .mp4 files to .webm
+It uses ffmpeg to render the video. So if your using this code in your website; besure to have ffmpeg installed.
+
+.mp4 ==> .webm
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MP4 to WebM Converter</title>
+</head>
+<body>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="video" accept=".mp4">
+        <button type="submit" name="convert">Convert to WebM</button>
+    </form>
+
+    <?php
+    if (isset($_POST['convert'])) {
+        if ($_FILES['video']['error'] === UPLOAD_ERR_OK) {
+            // Set the paths for the uploaded and converted files
+            $uploadedFile = $_FILES['video']['tmp_name'];
+            $convertedFile = 'converted.webm';
+
+            // Convert the uploaded file to WebM using FFmpeg
+            exec("ffmpeg -i $uploadedFile -c:v libvpx-vp9 -b:v 0 -crf 10 -c:a libopus $convertedFile");
+
+            // Prompt the user to download the converted file
+            if (file_exists($convertedFile)) {
+                header('Content-Type: video/webm');
+                header('Content-Disposition: attachment; filename="' . basename($convertedFile) . '"');
+                header('Content-Length: ' . filesize($convertedFile));
+
+                ob_clean();
+                flush();
+                readfile($convertedFile);
+
+                // Delete the temporary converted file
+                unlink($convertedFile);
+                exit();
+            } else {
+                echo "Error converting the file.";
+            }
+        } else {
+            echo "Error uploading the file.";
+        }
+    }
+    ?>
+</body>
+</html>
